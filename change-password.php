5158cdb13cfc7c0932e3cc72028a74c77dc48bc7
unknown <khristianangelo.tiu@gmail.com>
2025-11-21 23:18:38 +0800

    MindCare Final Form

--- change-password.php
+++ change-password.php
@@ -211,7 +211,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       color: var(--primary-teal);
     }
 
-
+    .sidebar .nav-link.active {
+      background-color: #5ad0be;
+      color: white;
+    }
+    
     .sidebar .nav-link.active {
       background-color: var(--primary-teal);
       color: white;
@@ -521,7 +525,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
           <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="8.5" cy="7" r="4"></circle><line x1="20" y1="8" x2="20" y2="14"></line><line x1="23" y1="11" x2="17" y2="11"></line></svg>
           MY APPOINTMENTS
         </a>
-        <a class="nav-link" href="profile.php">
+        <a class="nav-link active" href="profile.php">
           <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
           PROFILE
         </a>
